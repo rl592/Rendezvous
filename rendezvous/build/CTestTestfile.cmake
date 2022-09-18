dd7b0a383186a893a76824a7625741028ba5bd14
rl592 <91575065+rl592@users.noreply.github.com>
2022-09-18 05:23:42 -0400

    Add files via upload

---
+++ rendezvous/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/ruohan/rendezvous/src
+# Build directory: /home/ruohan/rendezvous/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_setup_tf")
+subdirs("robot_state_publisher")
+subdirs("description")
+subdirs("one_jackal")
+subdirs("test")
