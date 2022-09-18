9e593bfc53f79e8bf1e6123045f58dd3f40b7af6
rl592 <91575065+rl592@users.noreply.github.com>
2022-09-18 05:21:45 -0400

    Add files via upload

---
+++ rendezvous/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ruohan/rendezvous/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ruohan/rendezvous/devel;/opt/ros/noetic'.split(';') if '/home/ruohan/rendezvous/devel;/opt/ros/noetic' != '' else []
